34bf1c3e06d140ea1250c9753b0b713e8e988224
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-03-16 15:32:36 +0100

    Importing, filtering and formatting data (for Exercise 2+3)

--- assignment-2.py
+++ assignment-2.py
@@ -13,10 +13,12 @@ print('Daniel Lindberg, dlind16@student.sdu.dk')
 print('Emilie Bruun Therp, emthe15@student.sdu.dk')
 
 #%% Imports
-import os
+import os # To set dir
+import pprint # To print "stuff" pretty
 #import pandas as pd
 import json # To work with json files
 from textblob import TextBlob # To do Naïve Bayes classifification
+from datetime import datetime
 
 
 #%% Enviroment and Data imports
@@ -26,9 +28,39 @@ filename = 'icoData_19092018.json'
 with open(filename) as json_data:
     icoData = json.load(json_data) #Load data into a list
 
-icoData = [x for x in icoData if not len(x) == 1] # ???
 
+#%% Formatting data
+icoData = [x for x in icoData if not len(x) == 1]  # Filter list - remove empty dirs
 
+icoDataFiltPersonNum = []
+icoDataFiltReview = []
+icoDataFiltReviewNum = []
+
+i = 0
+personNum = 0
+reviewNum = 0
+for person in icoData:
+    personNum += 1 # Count next person
+    endDate = person['dates']["icoEnd"].split(" ")[0]
+    if endDate != '0000-00-00' and datetime.strptime(endDate, "%Y-%m-%d") < datetime.strptime("2018-10-19", "%Y-%m-%d"):
+        print(endDate)
+        print(i)
+        validDate = True
+    else:
+        validDate = False
+    if validDate:
+        for rating in person['ratings']:
+            if "review" in rating and len(rating["review"]) > 0:
+                reviewNum += 1 # Count valid review
+                icoDataFiltPersonNum.append(personNum)
+                icoDataFiltReviewNum.append(reviewNum)
+                icoDataFiltReview.append(rating['review'])
+    i+=1 # Increment instance counter
+#Loop all of icoData
+# Save dates - dates -> icoEnd
+# Save ratings review - ratings -> review
+# Format as dataframe
+# Filter end-dates after 19-10-2018
 
 #%% Excersice 1
 
