6006b0984ab5a0cd9b0c9208dc43d36b429697cc
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-03-16 11:55:37 +0100

    Added skeleton

--- assignment-2.py
+++ assignment-2.py
@@ -13,3 +13,41 @@ print('Daniel Lindberg, dlind16@student.sdu.dk')
 print('Emilie Bruun Therp, emthe15@student.sdu.dk')
 
 #%% Imports
+# Soon
+
+#%% Enviroment
+# Nothing yet
+
+#%% Excersice 1
+
+#%% Excersice 2
+
+#%% Excersice 2 a
+
+#%% Excersice 2 b
+
+#%% Excersice 2 c
+
+#%% Excersice 2 d
+#%% Excersice 2 d i
+
+#%% Excersice 2 d ii
+
+#%% Excersice 3
+
+#%% Excersice 3 part 1
+
+#%% Excersice 3 part 2
+
+#%% Excersice 3 part 3
+#%% Excersice 3 part 3 a
+
+#%% Excersice 3 part 3 b
+
+#%% Excersice 3 part 3 c
+
+#%% Excersice 3 part 3 d
+#%% Excersice 3 part 3 d i
+
+#%% Excersice 3 part 3 d ii
+
