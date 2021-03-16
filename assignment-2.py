287f1542e634a59ff088d22a319f53309a430cf8
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2021-03-16 11:49:38 +0100

    Added file with names and import section

---
+++ assignment-2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 16 11:45:48 2021
+
+@author: Max Festersen Hansen, Daniel Lindberg, Mads Duelund Dorka, Mathias Eriksen, Emilie Bruun Therp
+"""
+
+#%% Name and e-mail
+print('Mads Duelund Dorka, mador17@student.sdu.dk')
+print('Max Festersen Hansen, maxfh20@student.sdu.dk')
+print('Mathias Eriksen, merik17@student.sdu.dk')
+print('Daniel Lindberg, dlind16@student.sdu.dk')
+print('Emilie Bruun Therp, emthe15@student.sdu.dk')
+
+#%% Imports
